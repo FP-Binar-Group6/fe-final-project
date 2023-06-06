2cceb638f432594baf619bb5c6ca558ef9a2acd6
alpi syahri <alfisyahri0912@gmail.com>
2023-06-06 21:23:16 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+
+import Homepage from './pages/homepage/Homepage'
+import Login from './pages/login/Login'
+import Register from './pages/register/Register'
+import Search from './pages/search/Search'
+import Booking from './pages/booking/Booking'
+import History from './pages/history/History'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/login' element={<Login/>} />
+        <Route path='/register' element={<Register/>} />
+        
+        <Route path='/' element={<Homepage/>} />
+        <Route path='/search' element={<Search/>} />
+        <Route path='/booking' element={<Booking/>} />
+        <Route path='/history' element={<History/>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
