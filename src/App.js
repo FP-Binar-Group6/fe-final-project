d83cb01874989ca325f2c2463f70e776b52ad3ea
dmasspr174 <81064269+dmasspr174@users.noreply.github.com>
2023-06-16 17:35:32 +0700

    Dimas feature notifikasi page (#8)
    * add responsive and notificationPage
    
    * add responsive and notificationPage
    
    ---------
    
    Co-authored-by: Salshine <118276275+Salshine@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
 
-import Homepage from './pages/homepage/Homepage'
-import Login from './pages/login/Login'
-import Register from './pages/register/Register'
-import Search from './pages/search/Search'
-import Booking from './pages/booking/Booking'
-import History from './pages/history/History'
-import Navbar from './components/navbar/Navbar';
+import Homepage from "./pages/homepage/Homepage";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Search from "./pages/search/Search";
+import Booking from "./pages/booking/Booking";
+import History from "./pages/history/History";
+import Navbar from "./components/navbar/Navbar";
+import Notifikasi from "./pages/notifikasi/Notifikasi";
 
 function App() {
   return (
     <BrowserRouter>
-    <Navbar />
+      <Navbar />
       <Routes>
         <Route path='/login' element={<Login/>} />
         <Route path='/register' element={<Register/>} />
@@ -24,6 +25,7 @@ function App() {
         <Route path='/booking/payment' element={<Booking/>} />
         <Route path='/booking/success' element={<Booking/>} />
         <Route path='/history' element={<History/>} />
+        <Route path="/notifikasi" element={<Notifikasi />} />
       </Routes>
     </BrowserRouter>
   );
